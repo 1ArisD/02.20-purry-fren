9778183326892db403d41bb513e55960a0011c63
arisd <arisdunn@gmail.com>
2025-02-21 21:23:54 -0600

    Changed button

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 //console.log(cat.outerHTML);
 button.addEventListener("click", function () {
-  //console.log("It's working!");
+ if (cat.classList.contains("show")) {
+  //console.log("Yes");
+  cat.classList.remove("show");
+  button.innerText = "Wait, come back!";
+  button.classList.add("disappear");
+ } else {
+  //console.log("No");
   cat.classList.add("show");
+  button.innerText = "Shoo, cat!!";
+  button.classList.remove("disappear");
+ }
 });
+
